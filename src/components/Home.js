d095b18517143567f6ae180e0fb362745c880ee3
Mari Sinkkonen <amiliini@hotmail.com>
2021-03-19 15:46:44 +0200

    images and carousel strater

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,47 @@ import "./Component.css";
 
 function Home() {
   return (
-    <div className="divi">
-      <div className="sisältö">
-        <p>
-          TETSUA rakennus on Pirkanmaan alueella palveleva rakennusyritys. Olen
-          aloittanut yrittäjänä vuoden 2020 alussa. Rakennusalasta on kertynyt
-          monipuolinen osaaminen 13 vuoden aikana erilaisissa kohteissa, aina
-          pienestä pintaremontista vauriokorjausten kautta uudisrakentamiseen.
-          Uudisrakentamisesta, joka on lähinnä sydäntäni löytyy
-          kokonaisvaltaista osaamista niin omakotitaloista kuin autotalleista.
-          <h3>Ota yhteyttä:</h3>
-        </p>
+    <div className="tausta">
+      <div className="divi">
+        <div className="sisältö">
+          <p>
+            TETSUA rakennus on Pirkanmaan alueella palveleva rakennusyritys.
+            Olen aloittanut yrittäjänä vuoden 2020 alussa. Rakennusalasta on
+            kertynyt monipuolinen osaaminen 13 vuoden aikana erilaisissa
+            kohteissa, aina pienestä pintaremontista vauriokorjausten kautta
+            uudisrakentamiseen. Uudisrakentamisesta, joka on lähinnä sydäntäni
+            löytyy kokonaisvaltaista osaamista niin omakotitaloista kuin
+            autotalleista.
+            <h3>Ota yhteyttä:</h3>
+          </p>
+          <p>
+            {" "}
+            <strong>TETSUA rakennus</strong>
+          </p>
+          <p> Ville Sinkkonen</p>
+          <p>
+            <i class="fas fa-phone-alt">
+              {" "}
+              <a href="tel:+358504049421">050-404 9421</a>
+            </i>
+          </p>
+          <p>
+            {" "}
+            <a href="mailto:tetsuarakennus@gmail.com">
+              tetsuarakennus@gmail.com
+            </a>
+          </p>
+
+          <p>
+            <a
+              id="insta"
+              href="https://www.instagram.com/tetsuarakennus/"
+              target="_blank"
+            >
+              <i class="fab fa-instagram fa-2x"></i>
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Kylpyhuone.js
+++ src/components/Kylpyhuone.js
@@ -1,9 +1,55 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Image from "react-bootstrap/Image";
 
 function Kylpyhuone() {
   return (
-    <div>
-      <p>Kylpyhuone on kiva juttu</p>
+    <div className="tausta">
+      {" "}
+      <div className="divi">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.flickr.com/photos/75487768@N04/7510139430"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>{" "}
+      <img src="pesu1.jpg" alt="kylppärikuva"></img>
+      <Image src="holder.js/pesu1" fluid />
+      <Image src="http://placekitten.com/200/200" rounded />
     </div>
   );
 }
